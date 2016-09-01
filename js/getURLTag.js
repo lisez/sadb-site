20abe06fe4b72befebb98f2f4547b359697f2d4d
sujj <mm4324@gmail.com>
2016-09-01 11:03:25 +0800

    complete js/getURLTag.js

--- js/getURLTag.js
+++ js/getURLTag.js
@@ -10,8 +10,8 @@ function getParameterByName(name, url) {
 
 function getParameterByBookMark(name, url) {
   if (!url) url =window.location.href;
-  name = name.replace(/\[\]/g, '\\$&');
-  var regex = new RegExp("#" + name + "(=([^&#]*)|&|#|$)"),
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp("#" + name + "(=([^&]*)|&|$)"),
       results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return '';
