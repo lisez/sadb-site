5b251179e0c5007ce7e78c79d6fddfaa4846e6e3
sujj <mm4324@gmail.com>
2016-09-23 00:28:18 +0800

    add growth

--- singlepetsinfo.php
+++ singlepetsinfo.php
@@ -36,7 +36,7 @@ $item = IntSafe($_GET['petsid'],0,3);
 $sql = Array(
 sprintf('SELECT * FROM `sadb_pets` AS `t1` INNER JOIN `sadb_pets_ref` AS `t2` ON `t2`.`pets_reaid`=`t1`.`pets_rlid` WHERE `t1`.`pets_id`=%s;', $item),
 
-sprintf('SELECT `pets_id`,`pets_reaid`,`t1`.`pets_name_cht` AS `pets_name_cht`,`t1`.`pets_rarity`,`t1`.`pets_element`, `t1`.`pets_max_hp`,`t1`.`pets_max_atk`, `t1`.`pets_max_def`, `t1`.`pets_max_spd`,`ICON` FROM `sadb_pets` AS `t1` INNER JOIN `sadb_pets_ref` AS `t2` ON `t2`.`pets_reaid`=`t1`.`pets_rlid` WHERE (`t1`.`pets_id`= %s) OR (`t1`.`pets_species`= (SELECT `pets_species` FROM `sadb_pets` WHERE `pets_id`= %s AND `t1`.`pets_rarity` != 0 AND `t1`.`pets_element` NOT IN ("地地","水水","火火","風風")));', $item, $item));
+sprintf('SELECT `pets_id`,`pets_reaid`,`t1`.`pets_name_cht` AS `pets_name_cht`,`t1`.`pets_rarity`,`t1`.`pets_element`, `t1`.`pets_max_hp`,`t1`.`pets_max_atk`, `t1`.`pets_max_def`, `t1`.`pets_max_spd`,`ICON` FROM `sadb_pets` AS `t1` INNER JOIN `sadb_pets_ref` AS `t2` ON `t2`.`pets_reaid`=`t1`.`pets_rlid` WHERE (`t1`.`pets_id`= %s) OR (`t1`.`pets_species`= (SELECT `pets_species` FROM `sadb_pets` WHERE `pets_id`= %s AND `t1`.`pets_rarity` != 0));', $item, $item));
 
 /*輸出資料*/
 $sadb_raw = $sadb -> queryMulti(implode($sql,' '));
@@ -108,6 +108,62 @@ $thisPet -> skill['pas3'] = Array(
                               'eft'=>$sadb_row['pets_pas_3_eft']);
 
 $pageTitle=sprintf('%s%s——%s', $thisPet->info['element'], $thisPet->info['species'],$thisPet->showName('cht'));
+
+/*處理同系列寵物資料*/
+$twiceElePet=Array(
+  'id'=> -1,
+  'label'=>''
+);
+$fullSeresPetsHTML='';
+foreach ($sadb_raw[1] as $key => $value) {
+  /*except pets*/
+  if($value['pets_name_cht']=='0') continue;
+  /*handle twice element*/
+  $skipPetsElement = Array('地地','水水','火火','風風');
+  if(
+  mb_strlen($thisPet->info['element'],'utf-8')==1 && preg_match('/('.$thisPet->info['element'].')\1/',$value['pets_element']))
+  {
+    $twiceElePet['id']    = $value['pets_id'];
+    $twiceElePet['label'] = $value['pets_element'];
+  }
+  if(in_array($value['pets_element'],$skipPetsElement)) continue;
+  /*detect this id*/
+  $assignPointer='';
+  if($value['pets_reaid']==$thisPet->info['rlid']){
+    $assignPointer='today';
+  }
+  /*default HTML theme*/
+  $rowHTML='
+  <div class="div-table-tr %s">
+    <div class="div-table-td">%s</div>
+    <div class="div-table-td">%s</div>
+    <div class="div-table-td">%s</div>
+    <div class="div-table-td">%s</div>
+    <div class="div-table-td">%s</div>
+    <div class="div-table-td">%s</div>
+    <div class="div-table-td">%s</div>
+  </div>
+  ';
+  /*output HTML code*/
+  $iconHTML='<a href="/pets-%s/"><img src="%s"  class="img-rounded pets-series-thumbnails"></a>';
+  $imageURL=($value['ICON']!=''?'http://i.imgbox.com/'.$value['ICON']:'/assets/images/none.png');
+  $navHTML=sprintf($iconHTML, $value['pets_id'], $imageURL);
+  $fullHTML=sprintf($rowHTML, $assignPointer,
+                              $navHTML,
+                              $value['pets_name_cht'],
+                              round($value['pets_max_hp']/5+$value['pets_max_atk']+$value['pets_max_def']+$value['pets_max_spd'],3),
+                              $value['pets_max_hp'],
+                              $value['pets_max_atk'],
+                              $value['pets_max_def'],
+                              $value['pets_max_spd']);
+  $fullSeresPetsHTML.=$fullHTML;
+};
+
+/*處理雙屬性寵物資料*/
+$twiceEleHTML='';
+if($twiceElePet['id']!=-1){
+  $twiceEleHTML=sprintf('<span class="boss-cell" style="float:right;"><a href="/pets-%s">查詢雙屬性</a></span>',$twiceElePet['id']);
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -115,8 +171,9 @@ $pageTitle=sprintf('%s%s——%s', $thisPet->info['element'], $thisPet->info['sp
   <body>
     <main class="wrapper">
       {% include header.html %}
-  <script language="JavaScript" src="{{ '/js/petsInfoUse.js?101' | prepend: site.baseurl }}"></script>
+  <script language="JavaScript" src="{{ '/js/petsInfoUse.js' | prepend: site.baseurl }}"></script>
   <script language="JavaScript">
+    /*claim pets data*/
     var valStartLV=<?php echo $thisPet->showStartLV();?>;
     var valStart=[<?php echo implode($thisPet->pwr['start'],',');?>];
     var valMin=[<?php echo implode($thisPet->pwr['min'],',');?>];
@@ -151,6 +208,7 @@ $pageTitle=sprintf('%s%s——%s', $thisPet->info['element'], $thisPet->info['sp
     });
   </script>
       <article class="post container" itemscope itemtype="http://schema.org/BlogPosting">
+        <?php echo $twiceEleHTML;?>
         <header class="post-header">
           <h1 class="post-title" itemprop="name headline"><?php echo $pageTitle;?></h1>
         </header>
@@ -159,11 +217,11 @@ $pageTitle=sprintf('%s%s——%s', $thisPet->info['element'], $thisPet->info['sp
             <p>JavaScript無法正常啟用，將影響本頁部分內容顯示。請檢查是否有使用AdBlock等影響有JavaScript功能之擴充功能，或者升級瀏覽器至最新版本。如仍無法解決此問題，請洽本站管理員。</p>
           </div>
           <div class="row">
-            <!-- 寵物圖片-->
+            <!-- pet image-->
             <div class="col-xs-6 col-md-4">
               <img src="<?php echo $thisPet->showImg('profile');?>" class="img-responsive">
             </div>
-            <!-- 寵物基本資料-->
+            <!-- pet basic info-->
             <div class="col-xs-6 col-md-3">
               <div class='div-table'>
                 <div class='div-table-tr'>
@@ -219,26 +277,26 @@ $pageTitle=sprintf('%s%s——%s', $thisPet->info['element'], $thisPet->info['sp
                 </div>
                 <div class="div-table-tr">
                   <div class="div-table-th">體力</div>
-                  <div class="div-table-td"><input type="number" class="pet_hp_now"></div>
-                  <div class="div-table-td"><input type="number" class="pet_hp_char" placeholder="±??%"></div>
+                  <div class="div-table-td"><input type="number" step="0.1" class="pet_hp_now"></div>
+                  <div class="div-table-td"><input type="number" step="0.1" class="pet_hp_char" placeholder="±??%"></div>
                   <div class="div-table-td pet_hp_total"></div>
                 </div>
                 <div class="div-table-tr">
                   <div class="div-table-th">攻擊</div>
-                  <div class="div-table-td"><input type="number" class="pet_atk_now"></div>
-                  <div class="div-table-td"><input type="number" class="pet_atk_char" placeholder="±??%"></div>
+                  <div class="div-table-td"><input type="number" step="0.1" class="pet_atk_now"></div>
+                  <div class="div-table-td"><input type="number" step="0.1" class="pet_atk_char" placeholder="±??%"></div>
                   <div class="div-table-td pet_atk_total"></div>
                 </div>
                 <div class="div-table-tr">
                   <div class="div-table-th">防禦</div>
-                  <div class="div-table-td"><input type="number" class="pet_def_now"></div>
-                  <div class="div-table-td"><input type="number" class="pet_def_char" placeholder="±??%"></div>
+                  <div class="div-table-td"><input type="number" step="0.1" class="pet_def_now"></div>
+                  <div class="div-table-td"><input type="number" step="0.1" class="pet_def_char" placeholder="±??%"></div>
                   <div class="div-table-td pet_def_total"></div>
                 </div>
                 <div class="div-table-tr">
                   <div class="div-table-th">敏捷</div>
-                  <div class="div-table-td"><input type="number" class="pet_spd_now"></div>
-                  <div class="div-table-td"><input type="number" class="pet_spd_char" placeholder="±??%"></div>
+                  <div class="div-table-td"><input type="number" step="0.1" class="pet_spd_now"></div>
+                  <div class="div-table-td"><input type="number" step="0.1" class="pet_spd_char" placeholder="±??%"></div>
                   <div class="div-table-td pet_spd_total"></div>
                 </div>
               </div>
@@ -298,47 +356,25 @@ foreach ($_SkillRow as $value) {
             </div>
           </div>
 
-          <!-- 同系寵物-->
+          <!-- same series pets-->
           <div class="row" style="text-align:center;">
-            <div class="div-table">
+            <div class="div-table" id="petsSeries">
               <!-- pets attributes header -->
-              <div class="div-table-tr">
-                <div class="div-table-th">#</div>
-                <div class="div-table-th">名稱</div>
-                <div class="div-table-th">體力</div>
-                <div class="div-table-th">攻擊</div>
-                <div class="div-table-th">防禦</div>
-                <div class="div-table-th">敏捷</div>
-              </div>
-              <!-- pets data repeat -->
-              <?php
-              foreach ($sadb_raw[1] as $key => $value) {
-                /*except pets*/
-                if($value['pets_name_cht']=='0')continue;
-                /*default HTML theme*/
-                $rowHTML='
+              <div style="display:table-header-group;">
                 <div class="div-table-tr">
-                  <div class="div-table-td">%s</div>
-                  <div class="div-table-td">%s</div>
-                  <div class="div-table-td">%s</div>
-                  <div class="div-table-td">%s</div>
-                  <div class="div-table-td">%s</div>
-                  <div class="div-table-td">%s</div>
+                  <div class="div-table-th">#</div>
+                  <div class="div-table-th">名稱</div>
+                  <div class="div-table-th">成長率</div>
+                  <div class="div-table-th">體力</div>
+                  <div class="div-table-th">攻擊</div>
+                  <div class="div-table-th">防禦</div>
+                  <div class="div-table-th">敏捷</div>
                 </div>
-                ';
-                /*output HTML code*/
-                $iconHTML='<a href="/pets-%s/"><img src="%s"  class="img-rounded pets-series-thumbnails"></a>';
-                $imageURL=($value['ICON']!=''?'http://i.imgbox.com/'.$value['ICON']:'/assets/images/none.png');
-                $navHTML=sprintf($iconHTML, $value['pets_id'], $imageURL);
-                $fullHTML=sprintf($rowHTML, $navHTML,
-                                            $value['pets_name_cht'],
-                                            $value['pets_max_hp'],
-                                            $value['pets_max_atk'],
-                                            $value['pets_max_def'],
-                                            $value['pets_max_spd']);
-                echo $fullHTML;
-              };
-              ?>
+              </div>
+              <div style="display:table-row-group;">
+              <!-- pets data repeat -->
+              <?php echo $fullSeresPetsHTML; ?>
+              </div>
             </div>
           </div>
         </div>
